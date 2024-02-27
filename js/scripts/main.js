be04bb1a0b4019ca4e388c1c5f937a3f1bb84774
luuizz <luu.izz@hotmail.com>
2024-02-27 12:43:48 -0300

    create swiper slide post relative

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -6,3 +6,19 @@ btnMobile.addEventListener("click", () => {
     btnMobile.classList.toggle("is-active");
     document.documentElement.classList.toggle("menu-opened");
 })
+
+// Swiper Seção de posts relacionados
+
+var slidePostRel = new Swiper(".swiper-posts-rel", {
+    slidesPerView: 2,
+    spaceBetween: 32,
+    pagination: {
+        el: ".swiper-posts-rel .navigation .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-posts-rel .navigation .btn-next",
+        prevEl: ".swiper-posts-rel .navigation .btn-prev",
+    },
+    speed: 600,
+})
